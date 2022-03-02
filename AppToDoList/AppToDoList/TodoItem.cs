0a2a553eaaf210bc9fe837803ebf200c8ac64e6b
Eduardo Frasson Domine <eduardo.domine@etec.sp.gov.br>
2022-03-02 13:26:26 -0300

    Adicionar arquivos de projeto.

---
+++ AppToDoList/AppToDoList/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppToDoList
+{
+    public class TodoItem
+    {
+        public string TodoText { get; set; } 
+        public bool Complete { get; set; }
+
+        public TodoItem(string TodoText, bool Complete)
+        {
+            this.TodoText = TodoText;
+            this.Complete = Complete;   
+        }
+    }
+}
---
+++ AppToDoList/AppToDoList/TodoListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppToDoList
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+        }
+        
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));            
+        }
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+    }
+}
